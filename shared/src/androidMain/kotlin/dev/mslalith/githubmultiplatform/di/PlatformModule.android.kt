fc43d31112cd02f95fe15a8bda3e78a1e0b30b39
mslalith <mslalith.opensource@gmail.com>
2023-06-24 18:37:47 +0530

    chore: fix detekt issues

--- shared/src/androidMain/kotlin/dev/mslalith/githubmultiplatform/di/PlatformModule.android.kt
+++ shared/src/androidMain/kotlin/dev/mslalith/githubmultiplatform/di/PlatformModule.android.kt
@@ -1,7 +1,6 @@
 package dev.mslalith.githubmultiplatform.di
 
 import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
-import org.koin.dsl.module
 
 actual object PlatformModule {
     actual fun getSqlNormalizedCacheFactory() = SqlNormalizedCacheFactory("apollo.db")
