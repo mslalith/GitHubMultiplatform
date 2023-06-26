190cad95b09a99861815e62fd7c6a4555113c8b5
mslalith <mslalith.opensource@gmail.com>
2023-06-26 13:53:49 +0530

    chore: add Surface to GitHubApp

--- shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/ui/GitHubApp.kt
+++ shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/ui/GitHubApp.kt
@@ -1,6 +1,10 @@
 package dev.mslalith.githubmultiplatform.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import dev.mslalith.githubmultiplatform.ui.login.LoginScreen
 import dev.mslalith.githubmultiplatform.ui.theme.GitHubMultiplatformTheme
@@ -8,8 +12,13 @@ import dev.mslalith.githubmultiplatform.ui.theme.GitHubMultiplatformTheme
 @Composable
 fun GitHubApp() {
     GitHubMultiplatformTheme {
-        Navigator(
-            screen = LoginScreen()
-        )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Navigator(
+                screen = LoginScreen()
+            )
+        }
     }
 }
