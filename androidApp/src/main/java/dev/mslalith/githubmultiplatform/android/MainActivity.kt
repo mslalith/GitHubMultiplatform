8efcc7605cf0a843363bb9fa6fb6bea62a84af7b
mslalith <mslalith.opensource@gmail.com>
2023-06-27 15:08:46 +0530

    chore: forward deep link from Android to shared

--- androidApp/src/main/java/dev/mslalith/githubmultiplatform/android/MainActivity.kt
+++ androidApp/src/main/java/dev/mslalith/githubmultiplatform/android/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.mslalith.githubmultiplatform.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dev.mslalith.githubmultiplatform.deeplink.DeepLinkRegistry
 import dev.mslalith.githubmultiplatform.ui.GitHubApp
 
 class MainActivity : ComponentActivity() {
@@ -10,5 +11,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { GitHubApp() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        intent.data?.toString()?.let { DeepLinkRegistry.handle(deepLink = it) }
     }
 }
--- shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/di/Koin.kt
+++ shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/di/Koin.kt
@@ -3,14 +3,13 @@ package dev.mslalith.githubmultiplatform.di
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
-import org.koin.dsl.module
 
 fun initKoin(
-    moduleProvider: KoinApplication.() -> Module = { module {  } }
+    moduleProvider: (KoinApplication.() -> Module)? = null
 ) {
     startKoin {
+        moduleProvider?.let { modules(it()) }
         modules(
-            moduleProvider(),
             commonModule,
             networkModule,
             voyagerModule,
