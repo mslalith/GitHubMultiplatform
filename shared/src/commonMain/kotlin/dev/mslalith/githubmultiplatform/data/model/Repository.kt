9c58421bf58056b17bcacf5f730d112146149f4e
mslalith <mslalith.opensource@gmail.com>
2023-07-02 14:49:37 +0530

    chore: add repositoryTypeFilter functionality

--- shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/data/model/Repository.kt
+++ shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/data/model/Repository.kt
@@ -1,13 +1,16 @@
 package dev.mslalith.githubmultiplatform.data.model
 
 import dev.mslalith.githubmultiplatform.GetRepositoriesQuery
+import dev.mslalith.githubmultiplatform.platform.CommonSerializable
 
 data class Repository(
     val id: String,
     val name: String,
     val ownerName: String,
-    val ownerAvatarUrl: String
-)
+    val ownerAvatarUrl: String,
+    val isFork: Boolean,
+    val isPrivate: Boolean
+) : CommonSerializable
 
 fun GetRepositoriesQuery.Repositories.toPagedRepositories() = PagedRepositories(
     pageInfo = pageInfo.toPageInfo(),
@@ -18,7 +21,9 @@ fun GetRepositoriesQuery.Repositories.toPagedRepositories() = PagedRepositories(
             id = it.id,
             name = it.name,
             ownerName = ownerName,
-            ownerAvatarUrl = ownerAvatarUrl.toString()
+            ownerAvatarUrl = ownerAvatarUrl.toString(),
+            isFork = it.isFork,
+            isPrivate = it.isPrivate
         )
     }
 )
--- shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/ui/screens/repositorylist/RepositoryListScreenModel.kt
+++ shared/src/commonMain/kotlin/dev/mslalith/githubmultiplatform/ui/screens/repositorylist/RepositoryListScreenModel.kt
@@ -1,13 +1,18 @@
 package dev.mslalith.githubmultiplatform.ui.screens.repositorylist
 
+import androidx.compose.runtime.snapshotFlow
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
+import dev.mslalith.githubmultiplatform.data.model.Repository
 import dev.mslalith.githubmultiplatform.domain.usecase.GetRepositoriesUseCase
 import dev.mslalith.githubmultiplatform.platform.CommonSerializable
+import dev.mslalith.githubmultiplatform.ui.filters.repository.RepositoryTypeFilter
 import dev.mslalith.githubmultiplatform.ui.filters.repository.RepositoryTypeFilterState
 import dev.mslalith.githubmultiplatform.ui.screens.repositorylist.RepositoryListScreenState.Loading
 import dev.mslalith.githubmultiplatform.ui.screens.repositorylist.RepositoryListScreenState.Success
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
@@ -22,10 +27,27 @@ internal class RepositoryListScreenModel : StateScreenModel<RepositoryListScreen
     @delegate:Transient
     val repositoryTypeFilterState by inject<RepositoryTypeFilterState>()
 
+    private var repositories: List<Repository> = emptyList()
+
+    init {
+        snapshotFlow { repositoryTypeFilterState.selectedType }
+            .onEach(::updateReposByRepositoryTypeFilter)
+            .launchIn(scope = coroutineScope)
+    }
+
     fun fetchRepositories() {
         coroutineScope.launch {
-            val repos = getRepositoriesUseCase.run().firstOrNull()?.repositories.orEmpty()
-            mutableState.update { Success(repositories = repos) }
+            repositories = getRepositoriesUseCase.run().firstOrNull()?.repositories.orEmpty()
+            mutableState.update { Success(repositories = repositories) }
         }
+    }
+
+    private fun updateReposByRepositoryTypeFilter(filter: RepositoryTypeFilter) {
+        val repos = when (filter) {
+            RepositoryTypeFilter.ALL -> repositories
+            RepositoryTypeFilter.FORK -> repositories.filter { it.isFork }
+            RepositoryTypeFilter.PRIVATE -> repositories.filter { it.isPrivate }
+        }
+        mutableState.update { Success(repositories = repos) }
     }
 }
